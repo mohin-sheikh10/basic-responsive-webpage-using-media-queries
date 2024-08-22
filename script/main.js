504dc5d4bedf0c114aaad5e7aee81529c3285d11
mohin-sheikh10 <mohinshei014@gmail.com>
2024-08-23 00:57:54 +0600

    Basic Responsive Webpage desing using CSS Media Queries

---
+++ script/main.js
@@ -0,0 +1,14 @@
+const navlink = document.querySelector('.navlink');
+const menubar = document.querySelector('.menubar');
+const menubarIcon = document.getElementById('menubarIcon');
+
+menubar.addEventListener('click', function() {
+    navlink.classList.toggle('active');
+    menubarIcon.classList.toggle('fa-x')
+})
+
+window.onscroll = () => {
+    navlink.classList.remove('active');
+}
+
+
